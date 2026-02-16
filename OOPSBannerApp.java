1c880a0a5ca43fa3e1ed79aa904bc3bff5a6944c
Nandana A <na1812@srmist.edu.in>
2026-02-16 18:34:10 +0530

    Initial commit :Project setup with .gitignore

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ *
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Nandana A
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
+
